4bea425c35a595864ce9825440e1067ce25b0d7d
Jyothika-S <jyothi0351@gmail.com>
2024-03-07 12:29:16 +0530

    moved login data to a json file

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
+import loginData from '../test-data/loginData.json';
 
 //Page Obj Model
 test('login test', async ({ page }) => {
     const login = new LoginPage(page);
 
     await login.gotoLoginPage()
-    await login.login('jojo', 'Jyothi12!')
+    await login.login(loginData.username, loginData.password)
 
   });
